eb34dd4a90b9d701e2edc8e24aa41be1f7d1dc19
DanielRhee <danielrheewa@gmail.com>
2026-04-12 23:06:51 -0700

    finish quick rewrite i think

--- Integration/coneSegmentor.py
+++ Integration/coneSegmentor.py
@@ -46,13 +46,7 @@ class ConeSegmentor:
         if len(image.shape) != 3 or image.shape[2] != 3:
             raise ValueError("Input image must be a 3-channel RGB image (H, W, 3)")
 
-        results = self.model.predict(
-            source=image,
-            conf=self.confThreshold,
-            save=False,
-            device=self.device,
-            verbose=False
-        )
+        h, w = image.shape[:2]
 
         output = {
             'masks': None,
@@ -62,20 +56,37 @@ class ConeSegmentor:
             'numDetections': 0
         }
 
-        if len(results) == 0:
+        # Preprocessing (inline FastBaseTransform to avoid its hardcoded .cuda())
+        bgrImage = image[:, :, ::-1].copy()
+        frame = torch.from_numpy(bgrImage).to(self.device).float()
+        img = frame.unsqueeze(0).permute(0, 3, 1, 2).contiguous()
+        img = F.interpolate(img, (self.maxSize, self.maxSize), mode='bilinear', align_corners=False)
+
+        if self.backboneTransform.normalize:
+            img = (img - self.mean) / self.std
+        elif self.backboneTransform.subtract_means:
+            img = img - self.mean
+        elif self.backboneTransform.to_float:
+            img = img / 255
+
+        img = img[:, (2, 1, 0), :, :].contiguous()
+
+        extras = {"backbone": "full", "interrupt": False, "keep_statistics": False, "moving_statistics": None}
+        with torch.no_grad():
+            preds = self.model(img, extras=extras)["pred_outs"]
+
+        classes, scores, boxes, masks = postprocess(
+            preds, w, h, crop_masks=True, score_threshold=self.confThreshold
+        )
+
+        if classes.nelement() == 0:
             return output
 
-        result = results[0]
-
-        if result.masks is not None:
-            numDetections = len(result.masks)
-            output['numDetections'] = numDetections
-            output['masks'] = result.masks.data.cpu().numpy()
-            output['boxes'] = result.boxes.xyxy.cpu().numpy()
-            output['classes'] = [{
-                'id': int(cls),
-                'name': result.names[int(cls)]
-            } for cls in result.boxes.cls]
-            output['confidences'] = result.boxes.conf.cpu().numpy()
+        numDetections = classes.size(0)
+        output['numDetections'] = numDetections
+        output['masks'] = masks.cpu().float().numpy()
+        output['boxes'] = boxes.cpu().float().numpy()
+        output['classes'] = [{'id': int(c), 'name': self.classNames[c]} for c in classes]
+        output['confidences'] = scores.cpu().numpy()
 
         return output
